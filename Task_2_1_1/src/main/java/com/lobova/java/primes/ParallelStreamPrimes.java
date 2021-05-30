8ab9633a501e77a34ca702e61fa783d86eb04a9c
MargaritaLobova <lobova.rita.2002@gmail.com>
2021-05-31 02:05:29 +0700

    Initial Task_2_1_1 commit

---
+++ Task_2_1_1/src/main/java/com/lobova/java/primes/ParallelStreamPrimes.java
@@ -0,0 +1,29 @@
+package com.lobova.java.primes;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Parallel check with usage of the ParallelStream.
+ */
+public class ParallelStreamPrimes {
+    private static boolean hasNotPrime = false;
+
+    /**
+     * @param array of numbers that are needed to be checked.
+     * @return true if there is not prime number in the array(at least one), false if there are
+     * some prime numbers.
+     */
+    public static boolean streamRun(final Long[] array) {
+        List<Long> list = Arrays.asList(array);
+        Optional<Long> n = list
+                .parallelStream()
+                .filter(isPrime::isNotPrime)
+                .findFirst();
+        if (n.isPresent()) {
+            hasNotPrime = true;
+        }
+        return hasNotPrime;
+    }
+}
---
+++ Task_2_1_1/src/main/java/com/lobova/java/primes/ThreadPrime.java
@@ -0,0 +1,65 @@
+package com.lobova.java.primes;
+import java.util.Arrays;
+
+/**
+ * Parallel check with usage of the Thread class with the ability
+ * to specify the number of threads used.
+ */
+public class ThreadPrime {
+    static int THREADS = Runtime.getRuntime().availableProcessors();
+    static boolean hasNotPrime = false;
+    public static long[] arr;
+
+    /**
+     * @param array           of numbers that are needed to be checked.
+     * @param numberOfThreads if its needed, otherwise - all available processors.
+     * @return true if there is not prime number in the array(at least one), false if there are
+     * some prime numbers.
+     * @throws Exception
+     */
+    public static boolean threadRun(long[] array, int numberOfThreads) throws Exception {
+        if (numberOfThreads > 0 && numberOfThreads < THREADS)
+            THREADS = numberOfThreads;
+        Thread[] t = new Thread[THREADS];
+        arr = Arrays.copyOf(array, array.length);
+        for (int i = 0; i < THREADS; i++) {
+            t[i] = new Thread(new PrimeRun(i));
+            t[i].start();
+        }
+        for (int i = 0; i < THREADS; i++)
+            t[i].join();
+        return hasNotPrime;
+    }
+
+    public static long[] getArray() {
+        return arr;
+    }
+
+    public synchronized static void setHasNotPrime() {
+        hasNotPrime = true;
+    }
+}
+
+/**
+ * Class to work with threads.
+ */
+class PrimeRun implements Runnable {
+    final long[] array = ThreadPrime.getArray();
+    final int ID;
+
+    public PrimeRun(int i) {
+        ID = i;
+    }
+
+    /**
+     * Method to run thread check.
+     */
+    public void run() {
+        for (long l : array) {
+            if (l % ThreadPrime.THREADS == ID && isPrime.isNotPrime(l)) {
+                ThreadPrime.setHasNotPrime();
+                break;
+            }
+        }
+    }
+}
