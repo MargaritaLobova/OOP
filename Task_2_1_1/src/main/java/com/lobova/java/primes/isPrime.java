94a06ba71951b36b5f2dc001b61d80215e98d12d
MargaritaLobova <lobova.rita.2002@gmail.com>
2021-06-02 00:59:15 +0700

    Brases were missed

--- Task_2_1_1/src/main/java/com/lobova/java/primes/isPrime.java
+++ Task_2_1_1/src/main/java/com/lobova/java/primes/isPrime.java
@@ -15,9 +15,10 @@ public class IsPrime {
         if (n <= 1 || (n & 1) == 0) {
             return false;
         }
-        for (int i = 3; (long) i * i <= n; i += 2)
+        for (int i = 3; (long) i * i <= n; i += 2) {
             if (n % i == 0) {
-            return false;
+                return false;
+            }
         }
         return true;
     }
